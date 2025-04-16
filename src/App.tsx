3b787ba1bb5b2f508ac02e7d1d0011f6aa90aa2c
trirgg <triramdhani00@gmail.com>
2025-04-16 21:21:04 +0700

    inital setup vite+react+tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+     <div className='text-red-800 italic text-7xl' >Hewllo</div>
+    </>
+  )
+}
+
+export default App
